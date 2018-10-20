6d332bdd1d51bb884acd3e9a2686370031b9eca4
Dino Fizzotti <dino@dinofizzotti.com>
2018-10-20 08:02:56 +0200

    Cleaning up Django classes

--- carbalert/carbalert_django/admin.py
+++ carbalert/carbalert_django/admin.py
@@ -1,19 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 
 from .models import Thread, SearchPhrase
-
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from django.contrib.auth.models import User
 
 
 class EmailRequiredMixin(object):
     def __init__(self, *args, **kwargs):
         super(EmailRequiredMixin, self).__init__(*args, **kwargs)
-        # make user email field required
         self.fields["email"].required = True
 
 
@@ -43,4 +38,3 @@ admin.site.unregister(User)
 admin.site.register(User, EmailRequiredUserAdmin)
 admin.site.register(Thread)
 admin.site.register(SearchPhrase)
-# Register your models here.
